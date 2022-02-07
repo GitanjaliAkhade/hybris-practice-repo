d36057397b7e0fb2250019ea00971fd3a45ce9d2
GitanjaliAkhade <gitanjaliakhade2021@.com>
2022-02-07 15:24:10 +0530

    second commit

---
+++ src/main/java/com/sapient/hybris/ui/HelloWord.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sapient.hybris.ui;
+
+/**
+ * @author user
+ *
+ */
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWord");
+
+	}
+
+}
